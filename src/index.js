2dd0a42b1f374a83194416844b680fc20300b70f
suyeon <suyeon.jeong@atto-research.com>
2020-06-09 18:03:11 +0900

    add agent, middleware, ArticlePreview (03)

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from "react";
 import { applyMiddleware, createStore } from  'redux';
-
+import { promiseMiddleware } from './middleware';
 const defaultState = {
     appName: 'conduit',
     articles: null
 };
 
 const reducer = function (state = defaultState, action) {
+    switch (action.type) {
+        case 'HOME_PAGE_LOADED':
+            return { ...state, articles: action.payload.articles }
+    }
     return state;
 }
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(promiseMiddleware));
 
 /*
 react로 작성된 컴포넌트들을 Provider안에 넣으면
